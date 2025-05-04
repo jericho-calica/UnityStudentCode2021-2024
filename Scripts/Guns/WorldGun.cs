1e42497a3b04e4c26f8fa0618fd9902120afd0dc
jericho-calica <jerichoronquillocalica@gmail.com>
2025-05-04 16:09:33 +0800

    Delete Scripts directory

--- Scripts/Guns/WorldGun.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WorldGun : MonoBehaviour
-{
-    [SerializeField] public GameObject itselfModel;
-    [SerializeField] public bool isPrimaryWeapon;
-}
